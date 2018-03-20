15791f8215b23310727ad8e248b3b2b6001c7dfb
mailaw <mlaw534@gmail.com>
2018-03-19 23:55:12 -0400

    Completed part 7

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -12,17 +12,11 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-         return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
-
-
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
     def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
         return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
@@ -42,7 +36,4 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
